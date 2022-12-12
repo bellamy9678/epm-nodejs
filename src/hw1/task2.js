53721892cd33f591ea798e267db2ff0161d1e292
Mikhail Fursa <mikhail_fursa@epplkraw0171.home>
2022-12-12 21:30:07 +0100

    task 2

---
+++ src/hw1/task2.js
@@ -0,0 +1,32 @@
+const fs = require('node:fs');
+const csvtojson = require('csvtojson');
+const { pipeline } = require('node:stream');
+
+const readStream = fs.createReadStream('./assets/nodejs-hw1-ex1.csv');
+const writeStream = fs.createWriteStream('./gen/output1.txt');
+const writeStream2 = fs.createWriteStream('./gen/output2.txt');
+const converter = csvtojson({ delimiter: ';' });
+
+pipeline(
+  converter.fromStream(readStream),
+  writeStream,
+  (err) => {
+    if (err) {
+      console.error('Converting failed.', err);
+    } else {
+      console.log('Converting succeeded.');
+    }
+  }
+);
+
+const main = async () => {
+  try {
+    const data = await converter.fromFile('./assets/nodejs-hw1-ex1.csv');
+    writeStream2.write(JSON.stringify(data).slice(1, -1).replaceAll(',{', '\n{'));
+    console.log('Converting succeeded.');
+  } catch (err) {
+    console.error('Converting failed.', err);
+  }
+};
+
+main();
